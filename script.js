f1195ac617327b9dcd7b4e8316ddd9de21a92207
ranatalha0 <rqna.talha922@gmail.com>
2025-06-24 10:40:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
-  }
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>JS Wala Page</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>Welcome!</h1>
+  <p>Button dabao aur dekhte hain kya hota hai:</p>
+  <button onclick="showMessage()">Click Me!</button>
+  <p id="output"></p>
+
+  <script src="script.js"></script>
+</body>
+</html>
